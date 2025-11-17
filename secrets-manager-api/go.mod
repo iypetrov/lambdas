de769ce01f62a3e2fb6e4bc257d5e1b24d530105
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-11-18 00:11:15 +0200

    feat: add routing
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- secrets-manager-api/go.mod
+++ secrets-manager-api/go.mod
@@ -4,6 +4,8 @@ go 1.24.10
 
 require (
 	github.com/aws/aws-lambda-go v1.50.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
+	github.com/go-chi/chi/v5 v5.2.3
 	github.com/rs/zerolog v1.34.0
 )
 
--- secrets-manager-api/main.go
+++ secrets-manager-api/main.go
@@ -2,16 +2,39 @@ package main
 
 import (
 	"context"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	chiadapter "github.com/awslabs/aws-lambda-go-api-proxy/chi"
+    "github.com/go-chi/chi/v5"
+
 	"github.com/iypetrov/lambdas/secrets-manager-api/config"
 	"github.com/iypetrov/lambdas/secrets-manager-api/logger"
 )
 
-func Handler(ctx context.Context, event interface{}) (interface{}, error) {
+
+func init() {
+}
+
+func Handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log := logger.Get(ctx)
-	log.Info("Hello, received event is: %v", event)
-	return event, nil
+	log.Info("Handling request: %v", event.Path)
+
+	var chiLambda *chiadapter.ChiLambda
+	r := chi.NewRouter()
+	
+	r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from /test"))
+	})
+	
+	r.Get("/secrets/{name}", func(w http.ResponseWriter, r *http.Request) {
+		secretName := chi.URLParam(r, "name")
+		w.Write([]byte("Requested secret: " + secretName))
+	})
+
+	chiLambda = chiadapter.New(r)
+	return chiLambda.ProxyWithContext(ctx, event)
 }
 
 func main() {
