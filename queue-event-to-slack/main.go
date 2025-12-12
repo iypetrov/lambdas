8d29823d7739b8fdf9c09d1a7a15446b52697b24
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-12-12 11:32:49 +0200

    feat: log just needed info
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- queue-event-to-slack/main.go
+++ queue-event-to-slack/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,14 +12,28 @@ import (
 
 func Handler(ctx context.Context, event events.SQSEvent) (interface{}, error) {
 	log := logger.Get(ctx)
-	log.Info("Hello, received event is: %v", event)
-	for _, message := range event.Records {
-		log.Info("Message ID: %s", message.MessageId)
-		log.Info("Body: %s", message.Body)
-		log.Info("Attributes: %#v", message.Attributes)
-		log.Info("MessageAttributes: %#v", message.MessageAttributes)
-		log.Info("----")
+	for _, msg := range event.Records {
+		var s3Event events.S3Event
+		if err := json.Unmarshal([]byte(msg.Body), &s3Event); err != nil {
+			log.Error("Failed to parse S3 event from SQS message: %v", err)
+			continue
+		}
+
+		for _, rec := range s3Event.Records {
+			bucket := rec.S3.Bucket.Name
+			object := rec.S3.Object.Key
+			eventName := rec.EventName
+			time := rec.EventTime
+			log.Info(
+				"S3 Event: %s occurred on object %s in bucket %s at %s",
+				eventName,
+				object,
+				bucket,
+				time,
+			)
+		}
 	}
+
 	return event, nil
 }
 
